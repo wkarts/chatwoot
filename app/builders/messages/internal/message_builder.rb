6bc7e97c4876427fe6fe5866a2cf2ca7b00dc2be
wkarts <57051272+wkarts@users.noreply.github.com>
2024-07-12 04:56:22 -0300

    Update message_builder.rb
    I replaced extends with < to indicate that Messages::Internal::MessageBuilder inherits from Messages::MessageBuilder.
    
    I kept the perform method with the call to the super method, which invokes the perform method of the parent class (Messages::MessageBuilder).

--- app/builders/messages/internal/message_builder.rb
+++ app/builders/messages/internal/message_builder.rb
@@ -1,9 +1,6 @@
-class Messages::Internal::MessageBuilder
-  extends Messages::MessageBuilder
-
+class Messages::Internal::MessageBuilder < Messages::MessageBuilder
   def perform
     super
-
     @message
   end
 end
