9319df1ad8d4c76517a5191e20985443eb368367
wkarts <57051272+wkarts@users.noreply.github.com>
2024-07-19 21:57:57 -0300

    Update Unoapi.vue

--- app/javascript/dashboard/routes/dashboard/settings/inbox/channels/Unoapi.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/channels/Unoapi.vue
@@ -53,7 +53,7 @@
         <input
           v-model.trim="url"
           type="text"
-          placeholder="$t('INBOX_MGMT.ADD.WHATSAPP.URL.PLACEHOLDER')"
+          :placeholder="$t('INBOX_MGMT.ADD.WHATSAPP.URL.PLACEHOLDER')"
         />
         <span v-if="$v.url.$error" class="message">
           {{ $t('INBOX_MGMT.ADD.WHATSAPP.URL.ERROR') }}
@@ -62,13 +62,11 @@
     </div>
 
     <div class="w-[65%] flex-shrink-0 flex-grow-0 max-w-[65%] config-helptext">
-      <label
-        :class="'switch-label ' + { error: $v.ignoreHistoryMessages.$error }"
-      >
+      <label :class="{ error: $v.sendAgentName.$error }" style="display: flex; align-items: center;">
         <woot-switch
           v-model="sendAgentName"
           :value="sendAgentName"
-          class="switch"
+          style="flex: 0 0 auto; margin-right: 10px;"
         />
         {{ $t('INBOX_MGMT.ADD.WHATSAPP.SEND_AGENT_NAME.LABEL') }}
         <span v-if="$v.url.$error" class="message">
@@ -78,13 +76,11 @@
     </div>
 
     <div class="w-[65%] flex-shrink-0 flex-grow-0 max-w-[65%] config-helptext">
-      <label
-        :class="'switch-label ' + { error: $v.ignoreHistoryMessages.$error }"
-      >
+      <label :class="{ error: $v.ignoreGroupMessages.$error }" style="display: flex; align-items: center;">
         <woot-switch
           v-model="ignoreGroupMessages"
           :value="ignoreGroupMessages"
-          class="switch"
+          style="flex: 0 0 auto; margin-right: 10px;"
         />
         {{ $t('INBOX_MGMT.ADD.WHATSAPP.IGNORE_GROUPS.LABEL') }}
         <span v-if="$v.ignoreGroupMessages.$error" class="message">
@@ -94,13 +90,11 @@
     </div>
 
     <div class="w-[65%] flex-shrink-0 flex-grow-0 max-w-[65%] config-helptext">
-      <label
-        :class="'switch-label ' + { error: $v.ignoreHistoryMessages.$error }"
-      >
+      <label :class="{ error: $v.ignoreHistoryMessages.$error }" style="display: flex; align-items: center;">
         <woot-switch
           v-model="ignoreHistoryMessages"
           :value="ignoreHistoryMessages"
-          class="switch"
+          style="flex: 0 0 auto; margin-right: 10px;"
         />
         {{ $t('INBOX_MGMT.ADD.WHATSAPP.IGNORE_HISTORY.LABEL') }}
         <span v-if="$v.ignoreHistoryMessages.$error" class="message">
@@ -109,7 +103,21 @@
       </label>
     </div>
 
-    <div class="w-full" style="margin-top: 20px">
+    <div class="w-[65%] flex-shrink-0 flex-grow-0 max-w-[65%] config-helptext">
+      <label :class="{ error: $v.webhookSendNewMessages.$error }" style="display: flex; align-items: center;">
+        <woot-switch
+          v-model="webhookSendNewMessages"
+          :value="webhookSendNewMessages"
+          style="flex: 0 0 auto; margin-right: 10px;"
+        />
+        {{ $t('INBOX_MGMT.ADD.WHATSAPP.WEBWOOK_SEND_NEW_MESSAGES.LABEL') }}
+        <span v-if="$v.webhookSendNewMessages.$error" class="message">
+          {{ $t('INBOX_MGMT.ADD.WHATSAPP.WEBWOOK_SEND_NEW_MESSAGES.ERROR') }}
+        </span>
+      </label>
+    </div>
+
+    <div class="w-full" style="margin-top: 20px;">
       <woot-submit-button
         :loading="uiFlags.isCreating"
         :button-text="$t('INBOX_MGMT.ADD.WHATSAPP.SUBMIT_BUTTON')"
@@ -136,6 +144,7 @@ export default {
       ignoreGroupMessages: true,
       ignoreHistoryMessages: true,
       sendAgentName: true,
+      webhookSendNewMessages: true,
     };
   },
   computed: {
@@ -148,6 +157,7 @@ export default {
     ignoreGroupMessages: { required },
     ignoreHistoryMessages: { required },
     sendAgentName: { required },
+    webhookSendNewMessages: { required },
     url: { required },
   },
   methods: {
@@ -174,6 +184,7 @@ export default {
                 ignore_history_messages: this.ignoreHistoryMessages,
                 ignore_group_messages: this.ignoreGroupMessages,
                 send_agent_name: this.sendAgentName,
+                webhook_send_new_messages: this.webhookSendNewMessages,
                 url: this.url,
               },
             },
@@ -198,13 +209,3 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped>
-.switch {
-  flex: 0 0 auto;
-  margin-right: 10px;
-}
-.switch-label {
-  display: flex;
-  align-items: center;
-}
-</style>
