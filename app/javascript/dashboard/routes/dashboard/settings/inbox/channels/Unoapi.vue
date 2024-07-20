a6b3ce7fdfbbd6e11a38ceb4868751961ca5c134
wkarts <57051272+wkarts@users.noreply.github.com>
2024-07-20 13:59:41 -0300

    Update Unoapi.vue

--- app/javascript/dashboard/routes/dashboard/settings/inbox/channels/Unoapi.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/channels/Unoapi.vue
@@ -123,8 +123,20 @@
       </label>
     </div>
 
+    <div class="w-[65%] flex-shrink-0 flex-grow-0 max-w-[65%] config-helptext">
+      <woot-submit-button
+        :loading="uiFlags.isUpdating"          
+        :button-text="$t('INBOX_MGMT.ADD.WHATSAPP.GENERATE_API_KEY.LABEL')"
+        @click="generateToken"
+      /> 
+    </div>        
+    
     <div class="w-3/4 pb-4 config-helptext">
-
+      <woot-submit-button
+        :loading="uiFlags.isUpdating"          
+        :button-text="$t('INBOX_MGMT.ADD.WHATSAPP.GENERATE_API_KEY.LABEL')"
+        @click="generateToken"
+      /> 
     </div>
     
     <div class="w-full" style="margin-top: 20px">
@@ -132,13 +144,11 @@
         :loading="uiFlags.isCreating"
         :button-text="$t('INBOX_MGMT.ADD.WHATSAPP.SUBMIT_BUTTON')"
       />
-      <!--
       <woot-submit-button
-        :loading="uiFlags.isCreating"
-        :button-text="$t('INBOX_MGMT.ADD.WHATSAPP.GENERATE_API_KEY')"
+        :loading="uiFlags.isUpdating"          
+        :button-text="$t('INBOX_MGMT.ADD.WHATSAPP.GENERATE_API_KEY.LABEL')"
         @click="generateToken"
       /> 
-      -->
     </div>    
   </form>
 </template>
