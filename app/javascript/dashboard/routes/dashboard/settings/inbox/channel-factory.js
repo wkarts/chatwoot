0f1d596a612d4f554c47cf571639a18d17690666
wkarts <57051272+wkarts@users.noreply.github.com>
2024-08-22 00:36:06 -0300

    Update channel-factory.js

--- app/javascript/dashboard/routes/dashboard/settings/inbox/channel-factory.js
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/channel-factory.js
@@ -7,6 +7,7 @@ import Sms from './channels/Sms.vue';
 import Whatsapp from './channels/Whatsapp.vue';
 import Line from './channels/Line.vue';
 import Telegram from './channels/Telegram.vue';
+import Internal from './channels/Internal';
 
 const channelViewList = {
   facebook: Facebook,
@@ -18,6 +19,7 @@ const channelViewList = {
   whatsapp: Whatsapp,
   line: Line,
   telegram: Telegram,
+  internal: Internal,
 };
 
 export default {
