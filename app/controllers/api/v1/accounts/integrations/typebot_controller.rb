4f0d8f198837aa4bab9188baf4ef41fbe3e4286c
wkarts <57051272+wkarts@users.noreply.github.com>
2024-09-04 10:30:12 -0300

    Update typebot_controller.rb

--- app/controllers/api/v1/accounts/integrations/typebot_controller.rb
+++ app/controllers/api/v1/accounts/integrations/typebot_controller.rb
@@ -17,6 +17,19 @@ class Api::V1::Accounts::Integrations::TypebotController < Api::V1::Accounts::Ba
     render json: response, status: :ok
   end
 
+  def webhook
+    event = params[:event]
+    
+    case event
+    when 'widget_triggered'
+      handle_widget_triggered(params[:data])
+    when 'message_created'
+      handle_message_created(params[:data])
+    else
+      render json: { error: 'Unknown event' }, status: :bad_request
+    end
+  end
+
   private
 
   def set_account
@@ -25,5 +38,13 @@ class Api::V1::Accounts::Integrations::TypebotController < Api::V1::Accounts::Ba
 
   def set_integration_hook
     @integration_hook = @account.integration_hooks.find_by!(app_id: 'typebot')
+  end
+
+  def handle_widget_triggered(data)
+    TypebotService.new(@integration_hook).process_widget_triggered(data)
+  end
+
+  def handle_message_created(data)
+    TypebotService.new(@integration_hook).process_message_created(data)
   end
 end
