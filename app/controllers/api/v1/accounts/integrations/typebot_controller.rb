b8e77f75f8c8f106e38b359a7c531bd77942e740
wkarts <57051272+wkarts@users.noreply.github.com>
2024-09-07 15:28:28 -0300

    Update typebot_controller.rb

--- app/controllers/api/v1/accounts/integrations/typebot_controller.rb
+++ app/controllers/api/v1/accounts/integrations/typebot_controller.rb
@@ -1,26 +1,26 @@
 class Api::V1::Accounts::Integrations::TypebotController < Api::V1::Accounts::BaseController
   before_action :set_account
-  before_action :set_integration_hook
 
   def settings
-    render json: {
-      settings: @integration_hook.settings
-    }
+    render json: { settings: typebot_settings }
   end
 
-  def save_settings
-    @integration_hook.settings = params[:settings]
-    @integration_hook.save!
-    render json: { message: 'Settings saved successfully' }, status: :ok
+  def update_settings
+    @account.update!(typebot_settings: permitted_params)
+    render json: { settings: typebot_settings }
   end
 
   private
 
-  def set_account
-    @account = Current.account
+  def typebot_settings
+    @account.typebot_settings || {}
   end
 
-  def set_integration_hook
-    @integration_hook = @account.integration_hooks.find_by!(app_id: 'typebot')
+  def permitted_params
+    params.require(:settings).permit(:api_key, :project_id, :api_url, :frontend_url)
+  end
+
+  def set_account
+    @account = Current.account
   end
 end
